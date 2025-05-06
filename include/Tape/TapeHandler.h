b3052396bc3505c086d54b95f0a29f7872383cf3
VBStudi0s <vladybout@gmail.com>
2025-05-06 19:39:36 +0300

    Added TapeHandler skeleton

--- include/Tape/TapeHandler.h
+++ include/Tape/TapeHandler.h
@@ -1,8 +1,49 @@
 #pragma once
 
+#include <vector>
+#include <fstream>
+#include <string>
+
 #include "Tape/ITape.h"
 
+/*
+* Class implementing ITape interface using typical files.
+* Uses fstream to interact with file corresponding to tape.
+* Implements RAII idioma, creating fstream object in constructor 
+* and destroying it in destructor.
+* It is consideted that one tape can be read or written
+* only by one entity.
+*/
 class TapeHandler: public ITape
 {
+    std::fstream m_tape;    // fstream with corresponding tape file 
+    std::vector<int> m_tape_data;   // data of the tape
+    size_t m_pos = 0;   // current position on the tape
+public:
+    // Receives paths to files corresponding to the tape and settings
+    TapeHandler(std::string file_path, std::string settings_path = "");
 
+    // Reads data on current position
+    virtual int read() const override;
+
+    // Writes data on current position
+    virtual void write(int data) override;
+
+    // Moves one position left
+    virtual void left() override;
+
+    // Moves one position right
+    virtual void right() override;
+
+    // Returns true if it is start of the tape
+    virtual bool is_start() const override;
+
+    // Returns true if it is end of the tape
+    virtual bool is_end() const override;
+
+    // Returns true if tape in empty
+    virtual bool is_empty() const override;
+
+    // Writes data to the tape file
+    ~TapeHandler();
 };
--- sources/Tape/TapeHandler.cpp
+++ sources/Tape/TapeHandler.cpp
@@ -1,1 +1,59 @@
+#include <stdexcept>
+
 #include "Tape/TapeHandler.h"
+
+TapeHandler::TapeHandler(std::string file_path, std::string settings_path):
+    m_tape(file_path)
+{
+    // TODO: read settings
+
+    // TODO: read data
+}
+
+int TapeHandler::read() const
+{
+    // TODO: add delay
+    return m_tape_data.at(m_pos);
+}
+
+void TapeHandler::write(int data)
+{
+    // TODO: add delay
+    m_tape_data.at(m_pos) = data;
+}
+
+void TapeHandler::left()
+{
+    if(m_pos == 0)
+        throw std::runtime_error("Out of tape range movement while going left");
+    --m_pos;
+    // TODO: add delay
+}
+
+void TapeHandler::right()
+{
+    if(m_pos == m_tape_data.size() - 1)
+        throw std::runtime_error("Out of tape range movement while going left");
+    ++m_pos;
+        // TODO: add delay
+}
+
+bool TapeHandler::is_start() const
+{
+    return (m_pos == 0); 
+}
+
+bool TapeHandler::is_end() const
+{
+    return (m_pos == m_tape_data.size() - 1);
+}
+
+bool TapeHandler::is_empty() const
+{
+    return (m_tape_data.empty());
+}
+
+TapeHandler::~TapeHandler()
+{
+    //TODO: write data
+}
